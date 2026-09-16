bffdb01f4734b0150c775d0e15b874f490054c9e
Alaa-dev04 <nahea906.a@gmail.com>
2026-09-16 10:57:07 +0300

    this is the shearch api

---
+++ src/components/Providers.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import type { ReactNode } from "react";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
+
+const queryClient = new QueryClient();
+
+export default function Providers({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <NuqsAdapter>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </NuqsAdapter>
+  );
+}
